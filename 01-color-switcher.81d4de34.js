11e04cf8ebfb840196ab37f2bf120e2f4d249308
Ap01lY0n <128406680+Ap01lY0n@users.noreply.github.com>
2023-09-04 12:14:56 +0000

    Deploying to gh-pages from @ Ap01lY0n/goit-js-hw-09@6a6bc8ed405896964de7a2d3c7d41f16e5628606 🚀

---
+++ 01-color-switcher.81d4de34.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.body;let d=null;function l(){a.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`}t.addEventListener("click",(()=>{d||(d=setInterval(l,1e3),t.disabled=!0)})),e.addEventListener("click",(()=>{clearInterval(d),d=null,t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.81d4de34.js.map
